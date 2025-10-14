cf81f9edc9f319dc8215747986314803a44cf29b
Reuben <iamreubenokukusie@gmail.com>
2025-10-14 13:58:28 +0100

    added end points to my ASP.NET CORE identity project

---
+++ Api/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.DTOs.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "First name must be at least {2}, and maximum of {1} characters")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Last name must be at least {2}, and maximum of {1} characters")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address format")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Password must be at least {2} characters")]
+        public string Password { get; set; }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,17 @@
+using Api.Data;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +19,47 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 10;           // Good balance
+    options.Password.RequireDigit = true;           // At least one number
+    options.Password.RequireLowercase = true;       // At least one lowercase
+    options.Password.RequireUppercase = true;       // At least one uppercase  
+    options.Password.RequireNonAlphanumeric = true; // At least one special char
+
+    // Security settings
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+    options.SignIn.RequireConfirmedEmail = true;
+})
+     .AddRoles<IdentityRole>()
+     .AddRoleManager<RoleManager<IdentityRole>>()
+     .AddEntityFrameworkStores<Context>()
+     .AddSignInManager<SignInManager<User>>()
+     .AddUserManager<UserManager<User>>()
+     .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateIssuer = true,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -16,6 +71,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
